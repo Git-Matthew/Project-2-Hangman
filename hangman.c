7e633537d5570f6823ff6d3ede0c89b3ff72ca93
Git-Mekbot <mekbotpowerup@gmail.com>
2020-04-21 10:15:05 -0500

    Added more comments to better explain the code

--- hangman.c
+++ hangman.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Hangman//
+///Hangman///
 int main(int argc, char** argv)
 {
-    //Declare Variables//
+    ///Declare Variables///
     int number_of_letters = strlen(argv[1]);
     char hiddenword[number_of_letters];
     int lives;
     char letter;
     char *p;
 
-    //Counters//
+    ///Counters///
     int attempts = 0;
     int i;
     int j;
     int n;
 
-    //Letter Checkers//
+    ///Letter Checkers///
     int letter_a = 0;
     int letter_b = 0;
     int letter_c = 0;
@@ -46,38 +46,38 @@ int main(int argc, char** argv)
     int letter_y = 0;
     int letter_z = 0;
 
-    //Print Instructions//
+    ///Print Instructions///
     printf("\n Guess the word by typing a letter. \n");
     printf("\n List of words: \n");
-    printf("\n sponge, starfish, squid, crab, pineapple. \n");
+    printf("\n one, two, three, four, five. \n");
     printf("\n NOTE: Words are case sensitive. \n");
     
-    //Create The Hidden Word
-    for(j=0;j<=(number_of_letters-1);j++)
+    ///Create The Hidden Word///
+    for(j=0;j<=(number_of_letters-1);j++) //Fill hiddenword[] with a "j" ammount of "*".
     {
         hiddenword[j] = '*';
     }
-    printf("\n Word to guess: %s \n",hiddenword);
+    printf("\n Word to guess: %s \n",hiddenword); //Print a string of the same number of "*" as there are letters in the original word.
     
-    //Game For-Loop//
-    for(lives=5;lives>0;attempts++)
+    ///Game For-Loop///
+    for(lives=5;lives>0;attempts++) //Game continues until user loses 5 lives.
     {
-        //Special Counter//
-        n = 0;
+        ///Special Counter//
+        n = 0; //Resets the "n" counter which is the one that will keep track of if a letter was guessed correctly.
 
-        //Ask For A Letter//
+        ///Ask For A Letter///
         scanf(" %c", &letter);
 
-        //Check For Repeated Letters//
-        switch(letter)
-        {
+        ///Check For Repeated Letters///
+        switch(letter) //Checks if the user input is a letter in the alphabet.
+        {		//Also checks if the letter is repeated or not.
         case 'a':
-            letter_a++;
-            if(letter_a>1)
+            letter_a++; //Everytime a letter is used, it adds to a counter to make sure it doesnt get repeated.
+            if(letter_a>1) //If the letter is used more than once user loses 1 LifePoint.
             {
+	    lives--; //LifePoint -1.
             printf("\n You repeated a letter! -1 LifePoint. \n");
-            printf("\n You have %d LifePoints left. \n",lives);
-            lives--;
+            printf("\n You have %d LifePoints left. \n",lives); //Print current lives left.
             }
             break;
         case 'b':
@@ -307,34 +307,34 @@ int main(int argc, char** argv)
             break;
         }
 
-        //Check each letter of the word to see if it matches//
-        for(i=0;i<number_of_letters;i++)
+        ///Check each letter of the word to see if it matches///
+        for(i=0;i<number_of_letters;i++) //Compares user input to each possible letter from the original word.
         {
-            if(argv[1][i] == letter)
+            if(argv[1][i] == letter) //If it matches, it changes the "*" to the correct letter.
             {
                 hiddenword[i] = letter;
-                p = strchr(hiddenword,'*');
-                n++;
+                p = strchr(hiddenword,'*'); //Searches for occurances of "*", if there are none then "p" will be NULL.
+                n++; //Adds +1 to "n" if the letter was guessed correctly this round.
             }
         }
 
-        //Congratulate upon guessing a letter correctly//
-        if(n>0)
+        ///Congratulate upon guessing a letter correctly///
+        if(n>0) //Checks if a letter has been guessed correctly. (This will be reset after this round is over).
         {
-        if(p == NULL)
+        if(p == NULL) //If p == NULL it means there are no more "*" which means all the letters were guessed.
         {
             printf("\n Congratulations, you won!");
-            printf("\n The word was: \"%s\"\n",argv[1]);
-            return(0);
+            printf("\n The word was: \"%s\"\n",argv[1]); //Prints the correct word.
+            return(0); //Ends the program. (Because player already Won).
         }
         }
         else
         {
-        lives--;
+        lives--; //Removes -1 LifePoints from the player if the letter is not from the standard English alphabet or wrongly capitalized.
         printf("\n Incorrect letter! -1 LifePoint. \n Remember that the words are case sensitive. \n");
         printf("\n You have %d LifePoints left. \n",lives);
         }
-        printf("\n Word to guess: %s \n",hiddenword);
+        printf("\n Word to guess: %s \n",hiddenword); //Prints the word with "*" and the already guessed letters. (This happens each round).
     }
-    return(0);
+    return(0); //Ends the program. (Because player already lost).
 }
